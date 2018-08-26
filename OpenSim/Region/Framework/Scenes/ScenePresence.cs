bef2f923c63cbf16a3bd8fcdc91418309f5b2425
UbitUmarov <ajlduarte@sapo.pt>
2018-08-26 03:11:32 +0100

    mantis 8273 make change on crossings coerent

--- OpenSim/Region/Framework/Scenes/ScenePresence.cs
+++ OpenSim/Region/Framework/Scenes/ScenePresence.cs
@@ -1536,6 +1536,7 @@ namespace OpenSim.Region.Framework.Scenes
 */
         public int GetStateSource()
         {
+/*
             AgentCircuitData aCircuit = m_scene.AuthenticateHandler.GetAgentCircuitData(UUID);
 
             if (aCircuit != null && (aCircuit.teleportFlags != (uint)TeleportFlags.Default))
@@ -1546,6 +1547,8 @@ namespace OpenSim.Region.Framework.Scenes
                 return 5; // StateSource.Teleporting
             }
             return 2; // StateSource.PrimCrossing
+*/
+            return m_teleportFlags == TeleportFlags.Default ? 2 : 5;
         }
 
         /// <summary>
