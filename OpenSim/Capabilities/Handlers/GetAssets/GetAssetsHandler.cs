6750b1e3effdec460bc7ec807f00832fe17fbed1
lickx <sinesurfer@gmail.com>
2021-07-14 02:43:17 +0200

    Revert this code

--- OpenSim/Capabilities/Handlers/GetAssets/GetAssetsHandler.cs
+++ OpenSim/Capabilities/Handlers/GetAssets/GetAssetsHandler.cs
@@ -184,7 +184,20 @@ namespace OpenSim.Capabilities.Handlers
             response.ContentType = asset.Metadata.ContentType;
             response.RawBuffer = asset.Data;
             response.RawBufferLen = len;
-            response.Priority = 0;
+            if (type == AssetType.Mesh || type == AssetType.Texture)
+            {
+                if(len > 8196)
+                {
+                    //if(type == AssetType.Texture && ((asset.Flags & AssetFlags.AvatarBake)!= 0))
+                    //    responsedata["prio"] = 1;
+                    //else
+                    response.Priority = 2;
+                }
+                else
+                    response.Priority = 1;
+            }
+            else
+                response.Priority = -1;
         }
     }
 }
